b75e7e0907ea42138e0c328f262ed2a76c0c79aa
evantschetter-droid <evantschetter@outlook.com>
2026-05-30 23:54:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/api/convert', upload.single('image'), async (req, res) => {
 
     console.log('Calling Hugging Face...');
     const response = await fetch(
-     'https://api-inference.huggingface.co/models/lllyasviel/sd-controlnet-canny',
+     'https://router.huggingface.co/hf-inference/models/lllyasviel/sd-controlnet-canny',
       {
         method: 'POST',
         headers: {
